bb6d71d980d42b1c53c3e9d0a982687469d32cec
Zasekle <quickshiftx@gmail.com>
2023-12-15 07:01:04 -0500

    Covered eigendecomposition and some practical applications for eigendecomposition.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from data_structures_for_linear_algebra import data_structures_for_linear_algebr
 from tensor_operations import tensor_operations_fn
 from matrix_properties import matrix_properties_fn
 from eigenvectors_and_eigenvalues import eigenvectors_and_eigenvalues_fn
+from matrix_operations_for_machine_learning import matrix_operations_for_machine_learning_fn
 
 if __name__ == '__main__':
     data_structures_for_linear_algebra_fn()
     tensor_operations_fn()
     matrix_properties_fn()
     eigenvectors_and_eigenvalues_fn()
+    matrix_operations_for_machine_learning_fn()
 
 
---
+++ matrix_operations_for_machine_learning.py
@@ -0,0 +1,8 @@
+# pytorch
+# pytorch and tensorflow seem to be alternatives to each other
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def matrix_operations_for_machine_learning_fn():
+    print("matrix_operations_for_machine_learning_fn")
