ffce5e93098d9cca83e6e21cfb2348df84523f2b
Zasekle <quickshiftx@gmail.com>
2023-12-20 04:33:11 -0500

    Covered the definition of the moore-penrose pseudo inverse today.

--- matrix_operations_for_machine_learning.py
+++ matrix_operations_for_machine_learning.py
@@ -52,7 +52,9 @@ def data_compression_with_svd():
     U, sigma, V = np.linalg.svd(img_mat)
 
     # Rebuilt the image from the matrices. This will be a compressed version (less compressed depending on the value of
-    #  i).
+    #  i). Note that this is stored by variance. So the leftmost values inside the 'sigma' matrix will be the most
+    #  relevant. This allows for me to simply increase i below, including more columns and rows, and the image will get
+    #  more defined.
     i = 64
     reconstructing = np.matrix(U[:, :i]) * np.diag(sigma[:i]) * np.matrix(V[:i, :])
 
@@ -61,6 +63,36 @@ def data_compression_with_svd():
     plt.show()
 
 
+def the_moore_penrose_pseudo_inverse():
+    # Remember that when calculating the inverse of a matrix there are quite a few conditions such as the matrix must
+    #  be square. The moore-penrose pseudo inverse will take care of a lot of these problems.
+    # For some matrix A, its pseudo inverse A^+ can be calculated by:
+    #   A^+ = V * D^+ * U^T
+    #   U, D and V are SVD of A
+    #   D^+ = (D with reciprocal of all-non zero elements)^T
+
+    a = np.array([[-1, 2], [3, -2], [5, 7]])
+
+    u, d, v_t = np.linalg.svd(a)
+
+    print(u)
+    print(d)
+    print(v_t)
+
+    # Need to transform D into D^+
+    d_diag = np.diag(d)
+    d_inv = np.linalg.inv(d_diag)
+    d_plus = np.concatenate((d_inv, np.array([[0], [0]])), axis=1)
+
+    a_plus = np.dot(v_t.T, np.dot(d_plus, u.T))
+
+    print(a_plus)
+
+    # The built in method for calculating the pseudo inverse.
+    print(np.linalg.pinv(a))
+
+
 def matrix_operations_for_machine_learning_fn():
     singular_value_decomposition()
-    data_compression_with_svd()
+    # data_compression_with_svd()
+    the_moore_penrose_pseudo_inverse()
