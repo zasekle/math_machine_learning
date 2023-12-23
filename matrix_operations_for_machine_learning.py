6ae20ab76c5edd92f6cf340eaeff7883afbdf9dc
Zasekle <quickshiftx@gmail.com>
2023-12-23 05:11:28 -0500

    Used the moore-penrose pseudo inverse to find the regression of equations today.

--- matrix_operations_for_machine_learning.py
+++ matrix_operations_for_machine_learning.py
@@ -91,8 +91,71 @@ def the_moore_penrose_pseudo_inverse():
     # The built in method for calculating the pseudo inverse.
     print(np.linalg.pinv(a))
 
+    # For a man cannot serve two masters. He will come to despise one and love the other. This means that I cannot love
+    #  any other master besides God. So how do I put this in perspective with the other stuff? For example, does this
+    #  mean that I can't love my mother? I suppose that it doesn't mean that I can't LOVE any source other than God,
+    #  because it also says "Love your neighbor as yourself". It means I can't SERVE any other source. But does this
+    #  mean that I can't do things in order to work? I have to work, AND I should do my job as if unto the Lord. So
+    #  where does a Job fit in with this? Should I really be doing the bare minimum? That isn't doing my job as if unto
+    #  the Lord. Should I spend all my time doing my job? Then I will less (?) time to spend with the Lord. No, I think
+    #  a large part of my life is supposed to be toil. It is a curse from the garden if Eden. So, I guess it is ok to
+    #  work, but I need to keep the Lord at #1 in my life. NO, I need to NOT serve my job while still having a job and
+    #  doing it unto the Lord. So its because its all about the Lord. I serve him at all times, that means going and
+    #  getting a job because he tells me to, doing an AMAZING job because he tells me to and sharing the gospel because
+    #  he tells me to right? I think this is closer at least.
+
+
+def regression_with_the_pseudo_inverse():
+    # It is possible to do regression (the fitting to a line) using the pseudo inverse. Usually in real world data you
+    #  have more equations that you do unknowns (an overdetermined system), a lot more. Or in deep learning you have
+    #  more unknowns than you have equations (an underdetermined system). This means that it is likely that no solution
+    #  for the system exists.
+
+    x1 = [0, 1, 2, 3, 4, 5, 6, 7.]  # E.g.: Dosage of drug for treating Alzheimer's disease
+    y = [1.86, 1.31, .62, .33, .09, -.67, -1.23, -1.37]  # E.g.: Patient's "forgetfulness score"
+
+    x0 = np.ones(8)
+
+    # Essentially what is happening here is that any equation of the form y = mx OR in vector form y = mX must go
+    # through the origin. However, we don't want that limitation on our regression. So in order to do this we add the
+    # variable for the y-intercept. So we have y = mx + b OR in vector form you combine the unknowns into the vector and
+    # extract the constants in front of them so for example [[1, 2], [1, 3], [1, 4]] * [b, m]. This means that a column
+    # of ones must be added to represent the new variable b.
+    X = np.concatenate((np.matrix(x0).T, np.matrix(x1).T), axis=1)
+
+    w = np.dot(np.linalg.pinv(X), y)
+
+    print(w)
+
+    # Clear the current figure and axes
+    plt.clf()
+    plt.cla()
+
+    b = np.asarray(w).reshape(-1)[0]
+    m = np.asarray(w).reshape(-1)[1]
+
+    title = 'Clinical Trial'
+    x_label = 'Drug dosage (mL)'
+    y_label = 'Forgetfulness'
+    fig, ax = plt.subplots()
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    ax.scatter(x1, y)
+
+    x_min, x_max = ax.get_xlim()
+    y_at_x_min = m * x_min + b
+    y_at_x_max = m * x_max + b
+
+    ax.set_xlim([x_min, x_max])
+    _ = ax.plot([x_min, x_max], [y_at_x_min, y_at_x_max], c='C01')
+
+    plt.show()
+
 
 def matrix_operations_for_machine_learning_fn():
     singular_value_decomposition()
     # data_compression_with_svd()
     the_moore_penrose_pseudo_inverse()
+    regression_with_the_pseudo_inverse()
