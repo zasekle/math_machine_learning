c445e10cb416a654af9a050935874e3202a1152f
Zasekle <quickshiftx@gmail.com>
2024-01-12 08:10:13 -0500

    Started the calculus section. Covered the intro to differential calculus.

---
+++ limits.py
@@ -0,0 +1,16 @@
+# pytorch
+# pytorch and tensorflow seem to be alternatives to each other
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def intro_to_differential_calculus():
+    print("intro_to_differential_calculus")
+
+    # Calculus is the mathematical study of continuous change.
+    # The derivative is the slope of the line.
+    # Covered the basic stuff, velocity is the derivative of distance and acceleration is the derivative of velocity.
+
+
+def limits_fn():
+    intro_to_differential_calculus()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tensor_operations import tensor_operations_fn
 from matrix_properties import matrix_properties_fn
 from eigenvectors_and_eigenvalues import eigenvectors_and_eigenvalues_fn
 from matrix_operations_for_machine_learning import matrix_operations_for_machine_learning_fn
+from limits import limits_fn
 
 if __name__ == '__main__':
     data_structures_for_linear_algebra_fn()
@@ -10,5 +11,6 @@ if __name__ == '__main__':
     matrix_properties_fn()
     eigenvectors_and_eigenvalues_fn()
     matrix_operations_for_machine_learning_fn()
+    limits_fn()
 
 
