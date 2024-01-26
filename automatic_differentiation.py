65633c66f6e451412446392c9f604074ea9753a8
Zasekle <quickshiftx@gmail.com>
2024-01-26 08:26:41 -0500

    Working on an example for machine learning using automatic differentiation. It is not done yet and so I am not 100% sure what is going on with it.

--- automatic_differentiation.py
+++ automatic_differentiation.py
@@ -61,8 +61,41 @@ def autodiff_with_tensorflow():
     print(t.gradient(y, x))  # use autodiff
 
 
+def fitting_a_line_with_machine_learning():
+    # Using pytorch automatic differentiation library to fit a straight line to data points. y = mx + b
+
+    x = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7.])
+
+    # this will simulate a y value with some random noisy to be more realistic
+    y = -0.5 * x + 2 + torch.normal(mean=torch.zeros(8), std=0.2)
+
+    # set up data points that the model will need to do some work for
+    m = torch.tensor([0.9]).requires_grad_()
+    b = torch.tensor([0.1]).requires_grad_()
+
+    def regression(my_x, my_m, my_b):
+        return my_m * my_x + my_b
+
+    def regression_plot(my_x, my_y, my_m, my_b):
+        fig, ax = plt.subplots()
+
+        ax.scatter(my_x, my_y)
+
+        x_min, x_max = ax.get_xlim()
+        y_min = regression(x_min, my_m, my_b).detach().item()
+        y_max = regression(x_max, my_m, my_b).detach().item()
+
+        ax.set_xlim([x_min, x_max])
+        _ = ax.plot([x_min, x_max], [y_min, y_max])
+
+        plt.show()
+
+    regression_plot(x, y, m, b)
+
+
 def automatic_differentiation_fn():
     segment_intro()
     what_automatic_differentiation_is()
     autodiff_with_pytorch()
     autodiff_with_tensorflow()
+    fitting_a_line_with_machine_learning()
